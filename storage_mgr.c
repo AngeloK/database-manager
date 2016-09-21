dc1b88a9c96a67c857f75260905b0f07b1de9760
Neyanbhbin <kanng.yongjie@gmail.com>
2016-09-21 16:44:29 -0500

    complete remained functions.

--- storage_mgr.c
+++ storage_mgr.c
@@ -13,12 +13,16 @@
 #define TOTAL_PAGES 1000
 
 
+SM_FileHandle *fHandle;
+SM_PageHandle memPage;
 
 void initStorageManager (void) {
 
-	SM_FileHandle *fHandle = (SM_FileHandle *) malloc(sizeof(SM_FileHandle *));
+	fHandle = (SM_FileHandle *) malloc(sizeof(SM_FileHandle *));
+	memPage = (SM_PageHandle) malloc(sizeof(SM_PageHandle)); 
 
 };
+
 RC createPageFile (char *fileName) {
 
 	int mode = S_IRUSR | S_IWUSR;
@@ -46,7 +50,7 @@ RC createPageFile (char *fileName) {
 
 RC openPageFile (char *fileName, SM_FileHandle *fHandle) {
 
-	int flag = O_RDONLY;
+	int flag = O_RDWR|O_APPEND;
 	int fd = open(fileName, flag);
 
 	if (fcntl(fd, F_GETFL) < 0 && errno == EBADF) {
@@ -73,6 +77,7 @@ RC closePageFile (SM_FileHandle *fHandle) {
 	}
 	return RC_FILE_NOT_FOUND;
 };
+
 RC destroyPageFile (char *fileName) {
 	int r = remove(fileName);
 	if (r == 0) {
@@ -83,42 +88,146 @@ RC destroyPageFile (char *fileName) {
 
 /* reading blocks from disc */
 RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
-	if (pageNum < fHandle->totalNumPages) {
+	if (pageNum > (fHandle->totalNumPages)) {
 		return RC_READ_NON_EXISTING_PAGE;
 	}	
 
-	
+	off_t offset = pageNum * PAGE_SIZE;
+
+	// http://pubs.opengroup.org/onlinepubs/009695399/functions/read.html
+	if (pread(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) > 0 ) {
+		printf("%s\n", memPage);
+		return RC_OK;
+	}
+	else {
+		return RC_READ_NON_EXISTING_PAGE;
+	}
 
 
 }
-int getBlockPos (SM_FileHandle *fHandle);
-RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+int getBlockPos (SM_FileHandle *fHandle) {
+	return fHandle->curPagePos;
+}
+
+RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	if (read(fHandle->mgmtInfo, memPage, PAGE_SIZE) > 0) {
+		return RC_OK;
+	}
+	else {
+		return RC_READ_NON_EXISTING_PAGE;
+	}
+}
+
+RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	if (fHandle->curPagePos == 0) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}	
+	
+	off_t offset = (fHandle->curPagePos - 1) * PAGE_SIZE;
+
+	if (pread(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) < 0) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}
+
+	return RC_OK;
+}
+
+RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	off_t offset = fHandle->curPagePos * PAGE_SIZE;
+	if (pread(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) < 0) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}	
+	return RC_OK;
+}
+
+RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	if (fHandle->curPagePos == fHandle->totalNumPages) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}	
+	off_t offset = (fHandle->curPagePos + 1) * PAGE_SIZE;
+
+	if (pread(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) < 0) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}
+	return RC_OK;
+		
+}
+RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	off_t offset = fHandle->totalNumPages * PAGE_SIZE;
+	if (pread(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) < 0) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}	
+	return RC_OK;
+}
 
 /* writing blocks to a page file */
-RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-RC appendEmptyBlock (SM_FileHandle *fHandle);
-RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle);
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	if (pageNum > fHandle->totalNumPages) {
+		return RC_READ_NON_EXISTING_PAGE;
+	}	
 
+	off_t offset = pageNum * PAGE_SIZE;
 
+	// http://pubs.opengroup.org/onlinepubs/009695399/functions/read.html
+	if (pwrite(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) > 0 ) {
+		printf("%s\n", memPage);
+		return RC_OK;
+	}
+	return RC_WRITE_FAILED;
+	
+}
 
+RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	off_t offset = fHandle->curPagePos * PAGE_SIZE;
+	if (pwrite(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) < 0) {
+		return RC_WRITE_FAILED;
+	}	
+	return RC_OK;
+}
+
+RC appendEmptyBlock (SM_FileHandle *fHandle) {
+	
+	char data[PAGE_SIZE];
+	memset(data,'\0',sizeof(data));
+	
+	int fd = fHandle->mgmtInfo;
+
+	if(write(fd, memPage, PAGE_SIZE) < 0) {
+		return RC_WRITE_FAILED;
+	}
+	else {
+		return RC_OK;
+	}
+}
+
+RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle) {
+	if (fHandle->totalNumPages < numberOfPages) {
+		int diff = numberOfPages - fHandle->totalNumPages;
+		int i;
+		for (i = 0; i < diff; ++i) {
+			appendEmptyBlock(fHandle);	
+		}
+		return RC_OK;
+	}
+	return RC_OK;
+}
 
 int main(int argc, char *argv[]){
-	SM_FileHandle *fHandle = (SM_FileHandle *) malloc(sizeof(SM_FileHandle *));
-		
-	int c = createPageFile("helloworld.txt");
+
+	initStorageManager();
+
+	/*int c = createPageFile("helloworld.txt");*/
 	openPageFile("helloworld.txt", fHandle);
 
-	printf("%d\n", fHandle->curPagePos);
-	printf("%d\n", fHandle->totalNumPages);
-	printf("%p\n", fHandle->mgmtInfo);
+	/*printf("%d\n", fHandle->curPagePos);*/
+	/*printf("%d\n", fHandle->totalNumPages);*/
+	/*printf("%p\n", fHandle->mgmtInfo);*/
+
+	/*readBlock(0, fHandle, memPage);*/
+	appendEmptyBlock(fHandle);
 
 	int i = closePageFile(fHandle);
-	printf("%d\n", i);
+	/*printf("%d\n", i);*/
 
 	/*destroyPageFile("helloworld.txt");*/
 	return 0;

007b6bd3576f74478ad1de172d797bce3d3663c1
Neyanbhbin <kanng.yongjie@gmail.com>
2016-09-21 22:16:22 -0500

    added part of comments

--- storage_mgr.c
+++ storage_mgr.c
@@ -12,12 +12,13 @@
 
 #define TOTAL_PAGES 1000
 
-
+// Initialize page file handle and page handle.
 SM_FileHandle *fHandle;
 SM_PageHandle memPage;
 
 void initStorageManager (void) {
 
+	// allocate space for 2 handles.
 	fHandle = (SM_FileHandle *) malloc(sizeof(SM_FileHandle *));
 	memPage = (SM_PageHandle) malloc(sizeof(SM_PageHandle)); 
 
@@ -25,24 +26,30 @@ void initStorageManager (void) {
 
 RC createPageFile (char *fileName) {
 
-	int mode = S_IRUSR | S_IWUSR;
+	// mode defines the permisisons on a file descriptor
+	// S_IRUSR file owner has read, write, and execute permissions.
+	int mode = S_IRWXU;
 
-
+	// generate a new file descriptor.
 	int fd = creat(fileName, mode);
 
+	// fd is a non-negative integer if the file descriptor is generated successfully.
 	if (fd < 0) {
 		printf("file not found.\n");
 		return RC_FILE_NOT_FOUND;
 	}
 
+	// create a single page with '\0' bytes.
 	char data[PAGE_SIZE];
-
 	memset(data,'\0',sizeof(data));
 
+	// write the singe page to page file.
 	if (write(fd, data, PAGE_SIZE) != PAGE_SIZE) {
 		printf("Error writing to file %s\n", fileName);
 		return RC_WRITE_FAILED;
 	};
+
+	// close file descriptor.
 	close(fd);
 	return RC_OK;
 
@@ -50,19 +57,29 @@ RC createPageFile (char *fileName) {
 
 RC openPageFile (char *fileName, SM_FileHandle *fHandle) {
 
+	// flag defines file access mode.
+	// O_RDWR read and write mode.
+	// O_APPEND append mode, used for appending new block to page file.
 	int flag = O_RDWR|O_APPEND;
 	int fd = open(fileName, flag);
 
-	if (fcntl(fd, F_GETFL) < 0 && errno == EBADF) {
+	// fcntl gets or changes the file status, the seconed parameter "F_GETFL"
+	// is used for get the flag, if the file is opened correctly, it returns a 
+	// non-negative integer.
+	if (fcntl(fd, F_GETFL) < 0) {
 		return RC_FILE_NOT_FOUND;
 	}
 
+	// assign current fd to page file handle.
 	fHandle->mgmtInfo = (void *)fd;
 
+	// obtain file size via file descriptor.
 	struct stat sat;
 	fstat(fd, &sat);
 	int size = sat.st_size;
 	
+	// assign values to fileName, totalNumPages, and initialize current page 
+	// position as 0.
 	fHandle->fileName = fileName;
 	fHandle->totalNumPages = size/PAGE_SIZE;
 	fHandle->curPagePos = 0;
@@ -71,14 +88,20 @@ RC openPageFile (char *fileName, SM_FileHandle *fHandle) {
 }
 
 RC closePageFile (SM_FileHandle *fHandle) {
+
+	// access file descriptor by page file handle.
 	int fd = fHandle->mgmtInfo;
-	if (!close(fd)) {
+
+	// close function returns 0 if the file descriptor is closed.
+	if (close(fd) == 0) {
 		return RC_OK;
 	}
+	// otherwise, return error code. 
 	return RC_FILE_NOT_FOUND;
 };
 
 RC destroyPageFile (char *fileName) {
+	// destroy file.
 	int r = remove(fileName);
 	if (r == 0) {
 		return RC_OK;
@@ -88,12 +111,18 @@ RC destroyPageFile (char *fileName) {
 
 /* reading blocks from disc */
 RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	// if pageNum is greater than total number of pages in the file, return error.
 	if (pageNum > (fHandle->totalNumPages)) {
 		return RC_READ_NON_EXISTING_PAGE;
 	}	
 
+	// define offset used for finding and manipulating the particular page. 
 	off_t offset = pageNum * PAGE_SIZE;
 
+	// pread is used for reading PAGE_SIZE (here is 4096) bytes of data start
+	// from offset and assign it to memPage.
+	//
+	// detail of this function can be found here:
 	// http://pubs.opengroup.org/onlinepubs/009695399/functions/read.html
 	if (pread(fHandle->mgmtInfo, memPage, PAGE_SIZE, offset) > 0 ) {
 		return RC_OK;
